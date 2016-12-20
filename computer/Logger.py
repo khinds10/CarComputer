a30d55eae1f1c9f2eb7dbb40c932dfd99066cfd1
Kevin Hinds <khinds10.work@gmail.com>
2016-12-20 13:11:11 -0500

    get objects as JSON

--- computer/Logger.py
+++ computer/Logger.py
@@ -22,23 +22,18 @@ while True:
         localeInfo = data.getJSONFromDataFile('locale.data')
         if localeInfo == "":
             localeInfo = LocaleDetails.LocaleDetails()
-            localeInfo = json.loads(localeDetails.to_JSON())
+            localeInfo = json.loads(localeInfo.to_JSON())
         
         tempInfo = data.getJSONFromDataFile('temp.data')
         if tempInfo == "":
             tempInfo = CurrentReadings.CurrentReadings()
-            tempInfo = json.loads(currentReadings.to_JSON())
+            tempInfo = json.loads(tempInfo.to_JSON())
         
         weatherInfo = data.getJSONFromDataFile('weather.data')
         if weatherInfo == "":
             weatherInfo = WeatherDetails.WeatherDetails()
-            weatherInfo = json.loads(weatherDetails.to_JSON())
-        
-        print locationInfo
-        print localeInfo
-        print tempInfo
-        print weatherInfo
-        
+            weatherInfo = json.loads(weatherInfo.to_JSON())
+            
         postgres.saveDrivingStats(locationInfo, localeInfo, tempInfo, weatherInfo)    
     #except (Exception):
     #    pass
